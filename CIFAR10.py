c80e0f34f8ce1dfaacf0dce1434feb695931225e
VAMOT <tianhao.duan@ucdconnect.ie>
2024-01-02 22:12:43 +0800

    dropout

--- CIFAR10.py
+++ CIFAR10.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 from tensorflow import keras
 from VGG16 import MyVGG16
 from keras.datasets import cifar10
-import numpy as np
 
-using_gpu_index = 0  # 使用的 GPU 号码
+
+using_gpu_index = 0
 gpu_list = tf.config.experimental.list_physical_devices('GPU')
 if len(gpu_list) > 0:
     try:
@@ -17,12 +17,13 @@ if len(gpu_list) > 0:
 else:
     print("Got no GPUs")
 
+# data
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train / 255
 x_test = x_test / 255
 
 # model construction
-model = MyVGG16(10,fc1=256,fc2=128,input_shape=(32, 32, 3))
+model = MyVGG16(10,fc1=256,fc2=128,dp1=0,dp2=0,input_shape=(32, 32, 3))
 model.build((None,32,32,3))
 model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
               loss=keras.losses.SparseCategoricalCrossentropy(),
@@ -30,6 +31,6 @@ model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
 model.summary()
 
 # train
-model.fit(x_train, y_train, epochs=20, batch_size=32, validation_split=0.2)
+model.fit(x_train, y_train, epochs=100, batch_size=32)
 y_eva = model.evaluate(x_test, y_test, return_dict=True)
 
--- VGG16.py
+++ VGG16.py
@@ -1,12 +1,10 @@
 import tensorflow as tf
-from keras.datasets import cifar10
-from tensorflow import keras
-from keras import layers, Sequential, Model
+from keras import layers, Model
 
 
 class MyVGG16(Model):
     def __init__(self, classes: int, pooling: str = 'max', input_shape=None,
-                 fc1: int = 4096, fc2: int = 4096,
+                 fc1: int = 4096, fc2: int = 4096, dp1=0, dp2=0,
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -19,9 +17,9 @@ class MyVGG16(Model):
             pooling=pooling
         ).layers]
 
-        # full connection layers
-        self.fc1 = FC(fc1)
-        self.fc2 = FC(fc2)
+        # fully connected layers
+        self.fc1 = FC(fc1, dp1)
+        self.fc2 = FC(fc2, dp2)
         self.fc3 = layers.Dense(classes, activation="softmax")
 
     def call(self, inputs, training=None, mask=None):
@@ -35,15 +33,20 @@ class MyVGG16(Model):
 
 
 class FC(layers.Layer):
-    def __init__(self, num, **kwargs):
+    def __init__(self, num, dp=0, **kwargs):
         super().__init__(**kwargs)
 
         self.fc = layers.Dense(num)
         self.nb = layers.BatchNormalization()
         self.relu = layers.ReLU()
+        if dp == 0:
+            self.dp = lambda x: x
+        else:
+            self.dp = layers.Dropout(dp)
 
     def call(self, inputs, *args, **kwargs):
         outputs = self.fc(inputs)
         outputs = self.nb(outputs)
         outputs = self.relu(outputs)
+        outputs = self.dp(outputs)
         return outputs
